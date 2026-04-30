eb67fe3bbd4e0ba8d3014d291591d1f5d2f7b076
Vc <charan.vjc@gmail.com>
2026-04-30 22:57:13 +0530

    Update Untitled-1.py

--- test/Untitled-1.py
+++ test/Untitled-1.py
@@ -1,12 +1,1 @@
-"""print("hello world")
-print('hi')
-print("charan is a loser")
-for i in range(2):
-    print("we are acing this project!!!!")"""
-import mysql.connector as psq
-con=psq.connect(host='localhost',user='root',password='charan',database='sandhya',auth_plugin='mysql_native_password')
-cur=con.cursor()
-cur.execute("select * from stud where rollno=101;")
-l=cur.fetchall()
-print(l)
-con.commit()
+
