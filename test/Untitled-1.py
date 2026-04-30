3af53326b1f98fe20af32f2cc606cf5a659f936e
Vc <charan.vjc@gmail.com>
2026-04-30 22:41:03 +0530

    Update Untitled-1.py

--- test/Untitled-1.py
+++ test/Untitled-1.py
@@ -1,9 +1,10 @@
-"""print("hello world")
+''''print("hello world")
 print('hi')
 print("charan is a loser")
 for i in range(2):
-    print("we are acing this project!!!!")"""
+    print("we are acing this project!!!!")'''
 import mysql.connector as psq
 con=psq.connect(host='localhost',user='root',password='2101',database='sandhya')
 cur=con.cursor()
 cur.execute("select * from student where rollno=101;")
+ 
