08c4f164ed86ce53e37d48b52ab5b3afc1e7be02
Vc <charan.vjc@gmail.com>
2026-04-30 22:18:37 +0530

    Update Untitled-1.py

--- test/Untitled-1.py
+++ test/Untitled-1.py
@@ -3,8 +3,4 @@ print('hi')
 print("charan is a loser")
 for i in range(2):
     print("we are acing this project!!!!")
-<<<<<<< HEAD
-print("HAHAHAHA")
-=======
-print("helo")
->>>>>>> 67206ab033d450ed3c7b7e576c24c11b6849cf5f
+
