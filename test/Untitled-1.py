1388b114ee68a2307e7780468c21d5570f2fb8be
sandhyasriram2103 <sandhya.sriram2103@gmail.com>
2026-05-02 21:44:35 +0530

    h

--- test/Untitled-1.py
+++ test/Untitled-1.py
@@ -1,2 +1,9 @@
-print("hi")
+import mysql.connector as psq
+import mysql.connector as psq
+con=psq.connect(host='localhost',user='root',password='2101',database='sandhya')
+cur=con.cursor()
+def loginpg():
+    cur.execute("create table if not exist login(name char(30) not null,age int,gender char(1),email varchar(50),username varchar(30) primary key,password varchar(20) not null,bio varchar(20);")
+    con.commit()
+    con.close()
 
