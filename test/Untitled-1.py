111da0d9b104440460700607dfdc54d3bd805285
sandhyasriram2103 <sandhya.sriram2103@gmail.com>
2026-05-02 21:28:49 +0530

    Update Untitled-1.py

--- test/Untitled-1.py
+++ test/Untitled-1.py
@@ -1,9 +1,2 @@
-import mysql.connector as psq
-import os
-from dotenv import load_dotenv
-load_dotenv()
-con=psq.connect(host=os.getenv("DB_host"),user=os.getenv("DB_user"),password=os.getenv("DB_PASS"),database=os.getenv("DB_Name"))
-cur=con.cursor()
-cur.execute("desc student;")
-for x in cur:
-    print(x)
+print("hi")
+
