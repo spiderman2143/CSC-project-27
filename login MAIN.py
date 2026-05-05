61c2e4641bf9b72dda323aeccd1990284b5a2195
sandhyasriram2103 <sandhya.sriram2103@gmail.com>
2026-05-05 22:18:57 +0530

    S

--- login MAIN.py
+++ login MAIN.py
@@ -60,6 +60,33 @@ def user_signup():
 
     except psq.IntegrityError:
         messagebox.showerror("Error", "Username already exists!")
+# ---------------- ADMIN LOGIN FUNCTIONS ----------------
+
+def checkadmin(username, password):
+    con = psq.connect(**db_config)
+    cur = con.cursor()
+    cur.execute("SELECT * FROM admindetails WHERE username=%s AND password=%s", (username, password))
+    result = cur.fetchone()
+    con.close()
+    return result
+
+def ADlogin_action():
+    user = admin_user.get()
+    pw = admin_pass.get()
+    #EMPTY FIELD CHECK
+    if not all([user,pw]):
+        messagebox.showerror("Error", "All fields required")
+        return
+    #SECURE PASSWORD CHECK
+    if len(pw) <= 4:
+        messagebox.showerror("Error", "Password must be > 4 characters")
+        return
+    
+
+    if checkadmin(user, pw):
+        messagebox.showinfo("Success", "Admin Login successful!")
+    else:
+        messagebox.showerror("Failed", "Invalid Admin Credentials")
 
 # ---------------- LOGIN ----------------
 #CHECKING THE LOGIN DETAILS
@@ -74,6 +101,14 @@ def checklogin(username, password):
 def login_action():
     user = enteruser.get()
     pw = enterpass.get()
+    #EMPTY FIELD CHECK
+    if not all([user,pw]):
+        messagebox.showerror("Error", "All fields required")
+        return
+    #SECURE PASSWORD CHECK
+    if len(pw) <= 4:
+        messagebox.showerror("Error", "Password must be > 4 characters")
+        return
 
     if checklogin(user, pw):
         messagebox.showinfo("Success", "Login successful!")
@@ -91,9 +126,8 @@ base.geometry("1200x800")
 # Background
 try:
     img = Image.open("MOVIE.jpeg")
+    img = img.resize((1200, 800))
     bg_photo = ImageTk.PhotoImage(img)
-    img = img.resize((1920, 1080)) 
-
     bg_label = tk.Label(base, image=bg_photo)
     bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 except:
@@ -104,14 +138,20 @@ def show_signup_screen():
     login_frame.place_forget()
     signup_frame.place(relx=0.5, rely=0.5, anchor='center')
 
+def show_adminscreen():
+    login_frame.place_forget()
+    signup_frame.place_forget()
+    admin_frame.place(relx=0.5, rely=0.5, anchor='center')
+
 def show_login_screen():
+    admin_frame.place_forget()
     signup_frame.place_forget()
     login_frame.place(relx=0.5, rely=0.5, anchor='center')
 
 # ---------------- LOGIN FRAME ----------------
 login_frame = tk.Frame(base, bg='white', bd=2)
 
-tk.Label(login_frame, text="LOGIN", font=('Arial', 30, 'bold')).pack(pady=20)
+tk.Label(login_frame, text="USERLOGIN", font=('Arial', 30, 'bold')).pack(pady=20)
 
 tk.Label(login_frame, text="Username", font=('Arial', 15)).pack()
 enteruser = tk.Entry(login_frame, width=30)
@@ -123,6 +163,25 @@ enterpass.pack(pady=10)
 
 tk.Button(login_frame, text="Login", command=login_action).pack(pady=20)
 tk.Button(login_frame, text="Create Account", command=show_signup_screen).pack()
+
+#----------------ADMIN FRAME--------------
+#UI SETUP
+admin_frame = tk.Frame(base, bg='white', bd=2)
+
+tk.Label(admin_frame, text="ADMIN LOGIN", font=('Arial', 30, 'bold')).pack(pady=20)
+
+tk.Label(admin_frame, text="Username", font=('Arial', 15)).pack()
+admin_user = tk.Entry(admin_frame, width=30)
+admin_user.pack(pady=10)
+
+tk.Label(admin_frame, text="Password", font=('Arial', 15)).pack()
+admin_pass = tk.Entry(admin_frame, width=30, show="*")
+admin_pass.pack(pady=10)
+
+tk.Button(admin_frame, text="Login", command=ADlogin_action).pack(pady=20)
+tk.Button(login_frame, text="Admin Login", command=show_adminscreen).pack(pady=5)
+
+tk.Button(admin_frame, text="Back", command=show_login_screen).pack()
 
 # ---------------- SIGNUP FRAME ----------------
 signup_frame = tk.Frame(base, bg='white', bd=2)
